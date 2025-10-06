52a0c6879ad1d26670c609eb4333898f3d2c4a4e
Sanjuontheweb <sumukab3@gmail.com>
2025-10-06 21:55:51 +0530

    done! this program can make an arduino leds go pak pak pak

--- src/components/arduino.svelte
+++ src/components/arduino.svelte
@@ -1,7 +1,16 @@
 <script>
     import _ from 'lodash';
 
+    let port;
+    let writer; 
+    let status= "disconnected";
+
+    let usbvid = "0x2431";
+    let baudrate = "115200";
     let leds = _.range(0, 8).map(() => _.range(0, 8).map(() => false))
+
+    // a textEncoder which makes it easier for arduino to run
+    const enc = new TextEncoder();
 
     function onToggleLed(row, column) {
         leds[row][column] = !leds[row][column]
@@ -9,7 +18,35 @@
     }
     $: arduinoMsg = [...leds].reverse().reduce((acc, value) => {
         return acc + value.map(l => l?'1':'0').join(':') + ':';
-    }, '').slice(0, -1);
+    }, '').slice(0, -1) + "$";
+
+    // for this to work, you first need to turn on a flag in chrome : experimental web platform features
+    async function connectUsb() {
+        if (!navigator.serial) {
+            alert("Turn on 'Experimental Web Features' to connect to the arduino.");
+            return;
+        }
+
+        if (status === "connected") {
+            return;
+        }
+ 
+        try {
+            port = await navigator.serial.requestPort({
+                filters: [{ usbVendorId: {usbvid}}],
+            });
+            await port.open({ baudrate: {baudrate} });
+            writer = port.writable.getWriter();
+            status = "connected";
+        } catch (err) {
+            console.log(err);
+            alert("There was an error while connecting! I request an Apology");
+        }
+    }
+
+    $: if (status == "connected"  &&  arduinoMsg != "") {
+        writer.write(enc.encode(arduinoMsg));
+    }
 </script>
 
 <style>
@@ -36,12 +73,9 @@
     div.on {
         background-color: #AA0000;
     }
-    h1,h2 {
+    h1 {
         text-align: center;
         font-weight: 700;
-    }
-    h2 {
-        font-size: 30px;
     }
     section#message {
         width: 500px;
@@ -52,9 +86,43 @@
         font-weight: 700;
     }
 
+    #usb-connection-info {
+        width: 500px;
+        margin: 18px auto;
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        font-weight: 700;
+        gap: 10px;
+    }
+    #usb-connection-info h3 {
+        letter-spacing: 2px;
+    }
+    #usb-connection-info button {
+        border: none;
+        padding: 10px;
+        outline: none;
+        font-size: 16px;
+        cursor: pointer;
+        border-radius: 5px;
+    }
+    #usb-connection-info button:disabled {
+        cursor: not-allowed;
+    }
+    button:active,
+    button:focus {
+        outline: none;
+    }
+
 </style>
 
 <h1>LED MATRIX [ARDUINO]</h1>
+
+<section id="usb-connection-info">
+    <button disabled={status === "connected"} on:click={connectUsb}>Connect</button>
+    <h3>Status: {status}</h3>
+</section>
 
 <main>
     <!-- svelte-ignore a11y_click_events_have_key_events -->
@@ -67,8 +135,6 @@
         </section>
     {/each}
 </main>
-
-<h2>Adruino message</h2>
 
 <section id="message">
     {arduinoMsg}
